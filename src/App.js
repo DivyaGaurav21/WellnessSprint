201cb841b836643bf3d30a33ac3b42c3d1d0cdf1
Divya Gaurav <104293206+DivyaGaurav21@users.noreply.github.com>
2023-08-17 19:53:50 +0530

    add all required component setup for wellnessSprint

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dashboard />
+      <VirtualAdventureWorld />
+      <ExerciseChallenges />
+      <ProgressTracking />
+      <SocialInteraction />
+      <UserProfileSettings />
+      <WellnessResources />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
